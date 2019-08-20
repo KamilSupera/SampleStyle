c1ad4ea2f3523838803d3663b8638c5777211a6c
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-08-20 08:39:18 +0200

    Add codemirror

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/css/css';
+
 import Vue from 'vue'
+
 import App from './App'
 import router from './router'
 
 import './assets/styles.scss'
+
+/**
+ * To add new CodeMirror snippet by default use:
+ * `this.$CodeMirror.fromTextArea(<textarea element>, this.$CodeMirrorDefault);`
+ */
+Vue.prototype.$CodeMirror = CodeMirror;
+
+// Maybe make function to extend some of those arguments if needed.
+Vue.prototype.$CodeMirrorDefault = {
+  lineNumbers: true,
+  autofocus: false,
+  readOnly: true,
+  mode: 'css',
+  theme: 'solarized',
+}
 
 new Vue({
   router,
